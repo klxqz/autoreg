6d1dc90cb400db063ad0174e0d9644cf99548aa1
kl <kl@10.66.3.78>
2014-06-25 14:41:03 +0600

    Release

--- lib/actions/shopAutoregPluginFrontendChangePassword.controller.php
+++ lib/actions/shopAutoregPluginFrontendChangePassword.controller.php
@@ -4,9 +4,29 @@ class shopAutoregPluginFrontendChangePasswordController extends waJsonController
 
     public function execute() {
         try {
+            if (!wa()->getUser()->isAuth()) {
+                throw new waException("Ошибка авторизации");
+            }
+            $password = waRequest::post('password');
+            $new_password = waRequest::post('new_password');
+            $password_confirm = waRequest::post('password_confirm');
 
+            $contact = wa()->getUser();
+            if ($contact['password'] == waContact::getPasswordHash($password)) {
+                if (!$new_password) {
+                    throw new waException("Введите новый пароль");
+                }
+                if ($new_password != $password_confirm) {
+                    throw new waException("Введенные пароли не совпадают");
+                }
 
-            $this->response['message'] = "Пароль успешно изменен";
+                $contact->set('password', $new_password);
+                $contact->save();
+
+                $this->response['message'] = "Пароль успешно изменен";
+            } else {
+                throw new waException("Неверный пароль");
+            }
         } catch (Exception $e) {
             $this->setError($e->getMessage());
         }
--- lib/shopAutoreg.plugin.php
+++ lib/shopAutoreg.plugin.php
@@ -18,79 +18,78 @@ class shopAutoregPlugin extends shopPlugin {
         }
         $html = '';
 
-        if ($params['step'] == 'contactinfo') {
-            $template_path = wa()->getDataPath('plugins/autoreg/templates/ContactInfo.html', false, 'shop', true);
+        try {
+            if ($params['step'] == 'contactinfo') {
+                $template_path = wa()->getDataPath('plugins/autoreg/templates/ContactInfo.html', false, 'shop', true);
+                if (!file_exists($template_path)) {
+                    $template_path = wa()->getAppPath('plugins/autoreg/templates/ContactInfo.html', 'shop');
+                }
+                $view = wa()->getView();
+                $view->assign(array(
+                    'mailer_exists' => wa()->appExists('mailer'),
+                    'mailer_subscribe' => $this->getSettings('mailer_subscribe')
+                ));
+                $html .= $view->fetch($template_path);
+            }
+
+
+            if (wa()->getUser()->isAuth()) {
+                throw new waException("Пользователь уже авторизирован");
+            }
+            $checkout_data = wa()->getStorage()->get('shop/checkout');
+            $contact = isset($checkout_data['contact']) && ($checkout_data['contact'] instanceof waContact) ? $checkout_data['contact'] : new waContact();
+
+            $login = $contact->get('email', 'default');
+            if (!$login) {
+                throw new waException("Email не указан");
+            }
+
+            $email_validator = new waEmailValidator();
+            if (!$email_validator->isValid($login)) {
+                throw new waException("Неверный Email");
+            }
+
+            $contact_model = new waContactModel();
+            if ($contact_model->getByEmail($login, true)) {
+                throw new waException("Контакт с указанным Email уже существует");
+            }
+
+            $contact->set('create_method', 'autoreg-plugin');
+            $contact->set('create_ip', waRequest::getIp());
+
+            $contact->set('email', $login);
+            $password = $this->randomPassword();
+            $contact->set('password', $password);
+            $contact->save();
+            wa()->getAuth()->auth($contact);
+            //Добавляем подписку
+            if (wa()->appExists('mailer') && (($this->getSettings('mailer_subscribe') == 'manually' && waRequest::cookie('mailer_subscribe', 0)) || $this->getSettings('mailer_subscribe') == 'auto')) {
+                $this->addSubscribe($contact->getId(), $login);
+            }
+
+
+            $general = wa('shop')->getConfig()->getGeneralSettings();
+            $template_path = wa()->getDataPath('plugins/autoreg/templates/EmaiMessage.html', false, 'shop', true);
             if (!file_exists($template_path)) {
-                $template_path = wa()->getAppPath('plugins/autoreg/templates/ContactInfo.html', 'shop');
+                $template_path = wa()->getAppPath('plugins/autoreg/templates/EmaiMessage.html', 'shop');
             }
             $view = wa()->getView();
-            $view->assign(array(
-                'mailer_exists' => wa()->appExists('mailer'),
-                'mailer_subscribe' => $this->getSettings('mailer_subscribe')
-            ));
-            $html = $view->fetch($template_path);
-        }
-
-
-        if (wa()->getUser()->isAuth()) {
+            $view->assign(
+                    array(
+                        'contact' => $contact,
+                        'login' => $login,
+                        'password' => $password
+                    )
+            );
+            $notification = $view->fetch($template_path);
+            $message = new waMailMessage($this->getSettings('subject'), $notification);
+            $message->setTo($login);
+            $from = $general['email'];
+            $message->setFrom($from, $general['name']);
+            $message->send();
+        } catch (Exception $e) {
             return $html;
         }
-        $checkout_data = wa()->getStorage()->get('shop/checkout');
-        $contact = isset($checkout_data['contact']) && ($checkout_data['contact'] instanceof waContact) ? $checkout_data['contact'] : new waContact();
-
-        $login = $contact->get('email', 'default');
-        if (!$login) {
-            return $html;
-        }
-
-        $email_validator = new waEmailValidator();
-        if (!$email_validator->isValid($login)) {
-            return $html;
-        }
-
-        $contact_model = new waContactModel();
-        if ($contact_model->getByEmail($login, true)) {
-            return $html;
-        }
-
-        $contact->set('create_method', 'autoref-plugin');
-        $contact->set('create_ip', waRequest::getIp());
-
-        $contact->set('email', $login);
-        $password = $this->randomPassword();
-        $contact->set('password', $password);
-        $contact->save();
-        wa()->getAuth()->auth($contact);
-        //$this->setSessionData('contact', $contact);
-        //Добавляем подписку
-        if (wa()->appExists('mailer') && ($this->getSettings('mailer_subscribe') == 'manually' && waRequest::post('mailer_subscribe', 0)) || $this->getSettings('mailer_subscribe') == 'auto') {
-            try {
-                $this->addSubscribe($contact->getId(), $login);
-            } catch (Exception $ex) {
-                
-            }
-        }
-
-
-        $general = wa('shop')->getConfig()->getGeneralSettings();
-        $template_path = wa()->getDataPath('plugins/autoreg/templates/EmaiMessage.html', false, 'shop', true);
-        if (!file_exists($template_path)) {
-            $template_path = wa()->getAppPath('plugins/autoreg/templates/EmaiMessage.html', 'shop');
-        }
-        $view = wa()->getView();
-        $view->assign(
-                array(
-                    'contact' => $contact,
-                    'login' => $login,
-                    'password' => $password
-                )
-        );
-        $notification = $view->fetch($template_path);
-        $message = new waMailMessage($this->getSettings('subject'), $notification);
-        $message->setTo($login);
-        $from = $general['email'];
-        $message->setFrom($from, $general['name']);
-        $message->send();
     }
 
     protected function randomPassword() {
@@ -105,8 +104,7 @@ class shopAutoregPlugin extends shopPlugin {
     }
 
     public function addSubscribe($contact_id, $email) {
-
-
+        wa('mailer');
         // Remove contact from unsubscribers
         $um = new mailerUnsubscriberModel();
         $um->deleteByField(array(
@@ -119,11 +117,4 @@ class shopAutoregPlugin extends shopPlugin {
         $sm->add($contact_id, 1, $email);
     }
 
-    /*
-      protected function setSessionData($key, $value) {
-      $data = wa()->getStorage()->get('shop/cart', array());
-      $data[$key] = $value;
-      wa()->getStorage()->set('shop/cart', $data);
-      }
-     */
 }
